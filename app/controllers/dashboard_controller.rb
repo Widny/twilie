8f4cc6ab036ad1b31238b2fcdf26f626abf91b79
Widny <widnystlouis1@gmail.com>
2015-08-25 17:01:59 -0400

    first twilie commit

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,18 @@
+class DashboardController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+	
+
+  
+  def index
+  	response = Twilio::TwiML::Response.new do |r|
+  		#r.Say 'hello there', voice: 'alice'
+  		r.Dial action: "hello", record: "record-from-answer" do |d|
+  			d.Number "+19544961832" 
+  		end
+  	end
+  	render text: response.text
+  end
+
+  
+  
+end
